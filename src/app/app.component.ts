611dda105c4bf99e79b2c50da1ec0908308def85
Diego Lima <dicamposlima@gmail.com>
2020-01-03 13:05:09 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { Student } from './student/student.model';
+
+@Component({
+  selector: 'jad-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Corusant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'Han', isJedi: false}
+  ]
+
+}
